af060ca4054e77997b3ef1324b967df8f42b9e7e
bissaka- <bissaka-@student.s19.be>
2021-09-07 17:24:13 +0000

    Shell01 ajout de exo6

--- ex02/find_sh.sh
+++ ex02/find_sh.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-find . -name "*.sh" > txt # rechercher et mettre tout les fichier .sh dans le fichier txt
-# parcourire le fichier ligne par ligne, extraire l'extension et afficher
-cat txt | while  read ligne ; do
-  indice=$((${#ligne}-5))
-  echo ${ligne:2:$indice}
-done
-rm txt # suprimer le fichier txt 
+find . -name '*.sh' | sed 's/.*\///' | sed 's/.sh$//'
